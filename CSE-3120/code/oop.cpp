3c8f72e34d5beba9a2ec7abf95cd1eb7adc9065c
loloolk <marc.bondoger@gmail.com>
2023-02-08 12:45:59 -0700

    updated oop.cpp

--- CSE-3120/code/oop.cpp
+++ CSE-3120/code/oop.cpp
@@ -2,29 +2,29 @@
 #include <string>
 
 class Animal {
- private:
-  std::string name;
-  int age;
+private:
+	std::string name;
+	int age;
 
- public:
-  Animal(std::string name, int age) : name(name), age(age) {}
-  void setName(std::string name) { this->name = name; }
-  void setAge(int age) { this->age = age; }
-  std::string getName() { return name; }
-  int getAge() { return age; }
-  virtual void makeSound() { std::cout << "The animal makes a sound." << std::endl; }
+public:
+	Animal(std::string name, int age) : name(name), age(age) {}
+	void setName(std::string name) { this->name = name; }
+	void setAge(int age) { this->age = age; }
+	std::string getName() { return name; }
+	int getAge() { return age; }
+	virtual void makeSound() { std::cout << "The animal makes a sound." << std::endl; }
 };
 
 class Dog : public Animal {
- public:
-  Dog(std::string name, int age) : Animal(name, age) {}
-  void makeSound() { std::cout << "The dog barks." << std::endl; }
+public:
+	Dog(std::string name, int age) : Animal(name, age) {}
+	void makeSound() { std::cout << "The dog barks." << std::endl; }
 };
 
 class Cat : public Animal {
- public:
-  Cat(std::string name, int age) : Animal(name, age) {}
-  void makeSound() { std::cout << "The cat meows." << std::endl; }
+public:
+	Cat(std::string name, int age) : Animal(name, age) {}
+	void makeSound() { std::cout << "The cat meows." << std::endl; }
 };
 
 int main() {
